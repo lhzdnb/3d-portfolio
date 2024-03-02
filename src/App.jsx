8ea2cd252f568ebbceb23917f307d843fc919997
lh20020727 <lh20020727@sjtu.edu.cn>
2024-03-01 18:33:55 -0800

    accomplish hero page and nav bar

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Works,
+} from "./components/index.js";
+import Tech from "./components/Tech.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
