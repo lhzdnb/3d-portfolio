37b779198e8c3966a8192420e39f85237f4af0c6
lh20020727 <lh20020727@sjtu.edu.cn>
2024-03-02 02:23:24 -0800

    Updating contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_9l9idx4",
+        "template_mbnwz82",
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "lhzdnb",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "lhao20020727@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        "bvQj9e4-gvR8H8G2s",
       )
       .then(
         () => {
